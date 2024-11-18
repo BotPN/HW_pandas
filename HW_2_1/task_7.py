5e9121835d63a7a021294e12d242aa67fccb126c
BotPN <Bholiev@gmail.com>
2024-11-19 02:20:45 +0500

    Задание 7

---
+++ HW_2_1/task_7.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import random
+import string
+from task_5 import df_st
+
+product = input('Введите название товвара: ')
+
+print(f'Средний объем продаж раздела {product} равен {df_st[product].mean()} \n')
+
+df_markets=df_st[df_st[product] > df_st[product].mean()]
+
+print(f'Магазины объем продаж выше среднего \n {df_markets}')
